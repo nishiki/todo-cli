a2ca603b74539f0f3f25712de730ccbbcd243f13
nishiki <nishiki@yaegashi.fr>
2014-11-30 17:45:53 +0100

    add module TodoCli

--- lib/Cli.rb
+++ lib/Cli.rb
@@ -11,156 +11,158 @@ include CommandLineReporter
 require "#{APP_ROOT}/lib/Tasks"
 require "#{APP_ROOT}/lib/Config"
 
-class Cli
-
-	# Constructor
-	def initialize
-		@config = TodoCli::Config.new 
-		@tasks  = Tasks.new
-
-		if not @config.checkconfig
-			puts "#{I18n.t('display.error')} #1: #{@tasks.error_msg}"
-			exit(3)
-		end
-
-		if not @tasks.load
-			puts "#{I18n.t('display.error')} #1: #{@tasks.error_msg}"
-			exit(3)
-		end
-	end
-
-	# Add a new tasks
-	# @args: data -> the new task's data
-	def add(data={})
-		if @tasks.add(data)
-			if @tasks.save
-				puts I18n.t('success.add').green
-			else
-				puts "#{I18n.t('display.error')} #2: #{@tasks.error_msg}".red
-			end
-		else
-			puts "#{I18n.t('display.error')} #3: #{@tasks.error_msg}".red
-		end
-	end
-
-	# Show a task
-	# @args: id -> the tasks's id
-	def show(id)
-		task = @tasks.show(id)
-		if task.empty?
-			puts "#{I18n.t('diplay.error')} #4: #{@tasks.error_msg}".red
-		else
-			print "##{task['id']} - ".yellow
-			print "#{task['name']} - "
+module TodoCli
+	class Cli
 	
-			print "#{I18n.t('display.group')}: ".yellow
-			print "#{task['group']} - "
-
-			print "#{I18n.t('display.progress')}: ".yellow
-			puts "#{task['progress']}%".send(progress_color(task['progress']))
-
-			
-			puts '-' * 50
-
-			print "#{I18n.t('display.deadline')}: ".yellow
-			puts "#{task['deadline']}".send(deadline_color(task['deadline']))
-			print "#{I18n.t('display.comment')}: ".yellow
-			puts task['comment']
-
-			puts '-' * 50
-
-			print "#{I18n.t('display.date_create')}: ".yellow
-			puts task['date_create']
-			print "#{I18n.t('display.date_update')}: ".yellow
-			puts task['date_update']
-			print "#{I18n.t('display.date_finish')}: ".yellow
-			puts task['date_finish']
-
-			puts '-' * 50
-		end
-	end
-
-	# Update a task
-	# @args: id -> the tasks's id
-	#        data -> the new task's data
-	def update(id, data={})
-		if @tasks.update(id, data)
-			if @tasks.save
-				puts I18n.t('success.update', id: id).green
-			else
-				puts "#{I18n.t('display.error')} #6: #{@tasks.error_msg}".red
-			end
-		else
-			puts "#{I18n.t('display.error')} #7: #{@tasks.error_msg}".red
-		end
-	end
-
-	# Delete a tasks
-	# @args: id -> the tasks's id
-	def delete(id)
-		if @tasks.delete(id)
-			if @tasks.save
-				puts I18n.t('success.delete', id: id).green
-			else
-				puts "#{I18n.t('display.error')} #8: #{@tasks.error_msg}".red
-			end
-		else
-			puts "#{I18n.t('display.error')} #9: #{@tasks.error_msg}".red
-		end
-	end
-
-	# List all tasks
-	# @args: options -> filter for list
-	def list(options={})
-		table(border: false) do
-			row(header: true, color: 'yellow') do
-				column('ID', align: 'left', width: 5)
-				column(I18n.t('display.group'), width: 20, align: 'left')
-				column(I18n.t('display.name'), width: 40, align: 'left')
-				column(I18n.t('display.progress'), width: 20, align: 'left')
-				column(I18n.t('display.deadline'), width: 30, align: 'left')
+		# Constructor
+		def initialize
+			@config = TodoCli::Config.new 
+			@tasks  = TodoCli::Tasks.new
+	
+			if not @config.checkconfig
+				puts "#{I18n.t('display.error')} #1: #{@tasks.error_msg}"
+				exit(3)
 			end
 	
-			@tasks.list(options).each do |t|
-				task = t[1]
-			
-				row do
-					column(task['id'])
-					column(task['group'])
-					column(task['name'])
-					column("#{task['progress']}%", color: progress_color(task['progress']))
-					column(task['deadline'], color: deadline_color(task['deadline']))
+			if not @tasks.load
+				puts "#{I18n.t('display.error')} #1: #{@tasks.error_msg}"
+				exit(3)
+			end
+		end
+	
+		# Add a new tasks
+		# @args: data -> the new task's data
+		def add(data={})
+			if @tasks.add(data)
+				if @tasks.save
+					puts I18n.t('success.add').green
+				else
+					puts "#{I18n.t('display.error')} #2: #{@tasks.error_msg}".red
+				end
+			else
+				puts "#{I18n.t('display.error')} #3: #{@tasks.error_msg}".red
+			end
+		end
+	
+		# Show a task
+		# @args: id -> the tasks's id
+		def show(id)
+			task = @tasks.show(id)
+			if task.empty?
+				puts "#{I18n.t('diplay.error')} #4: #{@tasks.error_msg}".red
+			else
+				print "##{task['id']} - ".yellow
+				print "#{task['name']} - "
+		
+				print "#{I18n.t('display.group')}: ".yellow
+				print "#{task['group']} - "
+	
+				print "#{I18n.t('display.progress')}: ".yellow
+				puts "#{task['progress']}%".send(progress_color(task['progress']))
+	
+				
+				puts '-' * 50
+	
+				print "#{I18n.t('display.deadline')}: ".yellow
+				puts "#{task['deadline']}".send(deadline_color(task['deadline']))
+				print "#{I18n.t('display.comment')}: ".yellow
+				puts task['comment']
+	
+				puts '-' * 50
+	
+				print "#{I18n.t('display.date_create')}: ".yellow
+				puts task['date_create']
+				print "#{I18n.t('display.date_update')}: ".yellow
+				puts task['date_update']
+				print "#{I18n.t('display.date_finish')}: ".yellow
+				puts task['date_finish']
+	
+				puts '-' * 50
+			end
+		end
+	
+		# Update a task
+		# @args: id -> the tasks's id
+		#        data -> the new task's data
+		def update(id, data={})
+			if @tasks.update(id, data)
+				if @tasks.save
+					puts I18n.t('success.update', id: id).green
+				else
+					puts "#{I18n.t('display.error')} #6: #{@tasks.error_msg}".red
+				end
+			else
+				puts "#{I18n.t('display.error')} #7: #{@tasks.error_msg}".red
+			end
+		end
+	
+		# Delete a tasks
+		# @args: id -> the tasks's id
+		def delete(id)
+			if @tasks.delete(id)
+				if @tasks.save
+					puts I18n.t('success.delete', id: id).green
+				else
+					puts "#{I18n.t('display.error')} #8: #{@tasks.error_msg}".red
+				end
+			else
+				puts "#{I18n.t('display.error')} #9: #{@tasks.error_msg}".red
+			end
+		end
+	
+		# List all tasks
+		# @args: options -> filter for list
+		def list(options={})
+			table(border: false) do
+				row(header: true, color: 'yellow') do
+					column('ID', align: 'left', width: 5)
+					column(I18n.t('display.group'), width: 20, align: 'left')
+					column(I18n.t('display.name'), width: 40, align: 'left')
+					column(I18n.t('display.progress'), width: 20, align: 'left')
+					column(I18n.t('display.deadline'), width: 30, align: 'left')
+				end
+		
+				@tasks.list(options).each do |t|
+					task = t[1]
+				
+					row do
+						column(task['id'])
+						column(task['group'])
+						column(task['name'])
+						column("#{task['progress']}%", color: progress_color(task['progress']))
+						column(task['deadline'], color: deadline_color(task['deadline']))
+					end
 				end
 			end
 		end
-	end
-
-	private
-	def progress_color(progress)
-		case progress.to_i
-		when 100
-			return 'green'
-		when 1..99
-			return 'magenta'
-		else
-			return 'white'
-		end
-	end
-
-	private 
-	def deadline_color(deadline)
-		time     = Time.now
-		deadline = deadline.to_s
-		puts @config.deadline_limit
 	
-		if deadline.empty?
-			return 'white'
-		elsif Time.parse(deadline) < time
-			return 'red'
-		elsif Time.parse(deadline).to_i - time.to_i <= @config.deadline_limit
-			return 'magenta'
-		else
-			return 'white'
+		private
+		def progress_color(progress)
+			case progress.to_i
+			when 100
+				return 'green'
+			when 1..99
+				return 'magenta'
+			else
+				return 'white'
+			end
 		end
+	
+		private 
+		def deadline_color(deadline)
+			time     = Time.now
+			deadline = deadline.to_s
+		
+			if deadline.empty?
+				return 'white'
+			elsif Time.parse(deadline) < time
+				return 'red'
+			elsif Time.parse(deadline).to_i - time.to_i <= @config.deadline_limit
+				return 'magenta'
+			else
+				return 'white'
+			end
+		end
+
 	end
 end
--- lib/Tasks.rb
+++ lib/Tasks.rb
@@ -6,151 +6,154 @@ require 'yaml'
 require 'i18n'
 require 'time'
 
-class Tasks
-
-	attr_accessor :error_msg
-
-	# Constructor
-	def initialize
-		@error_msg = nil
-		@tasks     = {}
-		@file      = 'tasks.yml'
-
-		if /darwin/ =~ RUBY_PLATFORM
-			@dir_config = "#{Dir.home}/Library/Preferences/todo-cli"
-		elsif /cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM
-			@dir_config = "#{Dir.home}/AppData/Local/todo-cli"
-		else 
-			@dir_config = "#{Dir.home}/.config/todo-cli"
+module TodoCli
+	class Tasks
+	
+		attr_accessor :error_msg
+	
+		# Constructor
+		def initialize
+			@error_msg = nil
+			@tasks     = {}
+			@file      = 'tasks.yml'
+	
+			if /darwin/ =~ RUBY_PLATFORM
+				@dir_config = "#{Dir.home}/Library/Preferences/todo-cli"
+			elsif /cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM
+				@dir_config = "#{Dir.home}/AppData/Local/todo-cli"
+			else 
+				@dir_config = "#{Dir.home}/.config/todo-cli"
+			end
 		end
-	end
-
-	# Load data
-	# @rtrn: true if there is nothing error
-	def load
-		@tasks = YAML.load_file("#{@dir_config}/#{@file}") if File.exist?("#{@dir_config}/#{@file}")
-		return true
-	rescue Exception => e
-		@error_msg = "#{I18n.t('error.load')}\n#{e}"
-		return false
-	end
-
-	# Add a task
-	# @args: options -> name, comment, date_deadline
-	# @rtrn: false if the id is unknown
-	def add(options={})
-		if options[:name].to_s.empty?
-			@error_msg = I18n.t('error.name_empty')
+	
+		# Load data
+		# @rtrn: true if there is nothing error
+		def load
+			@tasks = YAML.load_file("#{@dir_config}/#{@file}") if File.exist?("#{@dir_config}/#{@file}")
+			return true
+		rescue Exception => e
+			@error_msg = "#{I18n.t('error.load')}\n#{e}"
+			return false
+		end
+	
+		# Add a task
+		# @args: options -> name, comment, date_deadline
+		# @rtrn: false if the id is unknown
+		def add(options={})
+			if options[:name].to_s.empty?
+				@error_msg = I18n.t('error.name_empty')
+				return false
+			end
+	
+			progress = options[:progress].to_i
+			progress = 0                              if not progress.between?(0, 100) 
+			deadline = Time.parse(options[:deadline]) if not options[:deadline].nil?
+	
+			date_finish = progress == 100 ? Time.now : nil
+			id          = @tasks.empty?   ? 1        : @tasks.to_a.last[0] + 1
+	
+			@tasks.merge!({id => {'id'          => id,
+			                      'name'        => options[:name],
+			                      'group'       => options[:group],
+			                      'deadline'    => deadline,
+			                      'progress'    => progress,
+			                      'comment'     => options[:comment],
+			                      'date_create' => Time.now,
+			                      'date_finish' => date_finish,
+			                     }
+			              }
+			             )
+	
+			return true
+		end
+	
+		# Remove a task
+		# @args: id -> the task's id
+		# @rtrn: false if the id is unknown
+		def delete(id)
+			id = id.to_i
+	
+			if not @tasks.has_key?(id)
+				@error_msg = I18n.t('error.id_unknown', id: id)
+				return false
+			end
+	
+			@tasks.delete(id)
+		end
+	
+		# Update a task
+		# @args: id -> the task's id
+		#        options -> name, comment, date_deadline
+		# @rtrn: false if the id is unknown
+		def update(id, options={})
+			id = id.to_i
+	
+			if not @tasks.has_key?(id)
+				@error_msg = I18n.t('error.id_unknown', id: id)
+				return false
+			end
+	
+			name     = options[:name].to_s.empty? ? @tasks[id]['name']     : options[:name]
+			group    = options[:group].nil?       ? @tasks[id]['group']    : options[:group]
+			comment  = options[:comment].nil?     ? @tasks[id]['comment']  : options[:comment]
+			progress = options[:progress].nil?    ? @tasks[id]['progress'] : options[:progress]
+			deadline = options[:deadline].nil?    ? @tasks[id]['deadline'] : options[:deadline]
+			progress = options[:progress].nil?    ? @tasks[id]['progress'] : options[:progress]
+	
+			deadline    = Time.parse(deadline.to_s) if not deadline.to_s.empty?
+			progress    = 0                         if not progress.to_i.between?(0, 100) 
+			date_finish = progress.to_i == 100 ? Time.now : nil
+	
+			@tasks.merge!({id => {'id'          => id,
+			                      'name'        => name,
+			                      'group'       => group,
+			                      'deadline'    => deadline,
+			                      'progress'    => progress,
+			                      'comment'     => comment,
+			                      'date_create' => @tasks[id]['date_create'],
+			                      'date_update' => Time.now,
+			                      'date_finish' => date_finish,
+			                     }
+			              }
+			             )
+			return true
+		end
+	
+		# Show all information a task
+		# @args: id -> the task's id
+		# @rtrn: return a hash
+		def show(id)
+			id = id.to_i
+			if not @tasks.has_key?(id)
+				@error_msg = I18n.t('error_id.unknown', id: id)
+				return {}
+			end
+	
+			return @tasks[id]
+		end
+	
+		# List all tasks
+		# @rtrn: return an array of hash
+		def list(options={})
+			tasks = @tasks.to_a
+			if not options[:group].nil?
+				tasks.delete_if {|t| t[1]['group'] != options[:group]}	
+			end
+	
+			return tasks
+		end
+	
+		# Save the tasks in file
+		# @rtrn: true if there is nothing error
+		def save
+			Dir.mkdir(@dir_config) if not Dir.exist?(@dir_config)
+	
+			File.open("#{@dir_config}/#{@file}", 'w') { |f| f << @tasks.to_yaml }
+			return true
+		rescue Exception => e
+			@error_msg = "#{I18n.t('error.save')}\n#{e}"
 			return false
 		end
 
-		progress = options[:progress].to_i
-		progress = 0                              if not progress.between?(0, 100) 
-		deadline = Time.parse(options[:deadline]) if not options[:deadline].nil?
-
-		date_finish = progress == 100 ? Time.now : nil
-		id          = @tasks.empty?   ? 1        : @tasks.to_a.last[0] + 1
-
-		@tasks.merge!({id => {'id'          => id,
-		                      'name'        => options[:name],
-		                      'group'       => options[:group],
-		                      'deadline'    => deadline,
-		                      'progress'    => progress,
-		                      'comment'     => options[:comment],
-		                      'date_create' => Time.now,
-		                      'date_finish' => date_finish,
-		                     }
-		              }
-		             )
-
-		return true
-	end
-
-	# Remove a task
-	# @args: id -> the task's id
-	# @rtrn: false if the id is unknown
-	def delete(id)
-		id = id.to_i
-
-		if not @tasks.has_key?(id)
-			@error_msg = I18n.t('error.id_unknown', id: id)
-			return false
-		end
-
-		@tasks.delete(id)
-	end
-
-	# Update a task
-	# @args: id -> the task's id
-	#        options -> name, comment, date_deadline
-	# @rtrn: false if the id is unknown
-	def update(id, options={})
-		id = id.to_i
-
-		if not @tasks.has_key?(id)
-			@error_msg = I18n.t('error.id_unknown', id: id)
-			return false
-		end
-
-		name     = options[:name].to_s.empty? ? @tasks[id]['name']     : options[:name]
-		group    = options[:group].nil?       ? @tasks[id]['group']    : options[:group]
-		comment  = options[:comment].nil?     ? @tasks[id]['comment']  : options[:comment]
-		progress = options[:progress].nil?    ? @tasks[id]['progress'] : options[:progress]
-		deadline = options[:deadline].nil?    ? @tasks[id]['deadline'] : options[:deadline]
-		progress = options[:progress].nil?    ? @tasks[id]['progress'] : options[:progress]
-
-		deadline    = Time.parse(deadline.to_s) if not deadline.to_s.empty?
-		progress    = 0                         if not progress.to_i.between?(0, 100) 
-		date_finish = progress.to_i == 100 ? Time.now : nil
-
-		@tasks.merge!({id => {'id'          => id,
-		                      'name'        => name,
-		                      'group'       => group,
-		                      'deadline'    => deadline,
-		                      'progress'    => progress,
-		                      'comment'     => comment,
-		                      'date_create' => @tasks[id]['date_create'],
-		                      'date_update' => Time.now,
-		                      'date_finish' => date_finish,
-		                     }
-		              }
-		             )
-		return true
-	end
-
-	# Show all information a task
-	# @args: id -> the task's id
-	# @rtrn: return a hash
-	def show(id)
-		id = id.to_i
-		if not @tasks.has_key?(id)
-			@error_msg = I18n.t('error_id.unknown', id: id)
-			return {}
-		end
-
-		return @tasks[id]
-	end
-
-	# List all tasks
-	# @rtrn: return an array of hash
-	def list(options={})
-		tasks = @tasks.to_a
-		if not options[:group].nil?
-			tasks.delete_if {|t| t[1]['group'] != options[:group]}	
-		end
-
-		return tasks
-	end
-
-	# Save the tasks in file
-	# @rtrn: true if there is nothing error
-	def save
-		Dir.mkdir(@dir_config) if not Dir.exist?(@dir_config)
-
-		File.open("#{@dir_config}/#{@file}", 'w') { |f| f << @tasks.to_yaml }
-		return true
-	rescue Exception => e
-		@error_msg = "#{I18n.t('error.save')}\n#{e}"
-		return false
 	end
 end
